ff8070b2751c07e3009e2a9d8ff1bfb0e15851d9
sat7070 <sat7070@gmail.com>
2013-08-14 22:57:14 +0200

    New files

--- Projets/Server/src/main.c
+++ Projets/Server/src/main.c
@@ -1,30 +1,65 @@
-/*#include <stdio.h>
-#include <Ecore.h>
-#include <Ecore_Con.h>
-*/
 #include<Elementary.h>
 
-#ifdef HAVE_CONFIG_H
-# include "config.h"
-#else
-# define __UNUSED__
-#endif
+
+Eina_Bool
+_add(void *data , int type, Ecore_Con_Event_Client_Add *ev)
+{
+  /* char welcome[] = "hello! - sent from the server";
+   Ecore_Con_Server *srv;
+   Ecore_Con_Client *cl;
+   const Eina_List *clients, *l;
+
+   struct _Client *client = malloc(sizeof(*client));
+   client->sdata = 0;
+
+   printf("Client with ip %s, port %d, connected = %d!\n",
+          ecore_con_client_ip_get(ev->client),
+          ecore_con_client_port_get(ev->client),
+          ecore_con_client_connected_get(ev->client));
+
+   ecore_con_client_send(ev->client, welcome, sizeof(welcome));
+   ecore_con_client_flush(ev->client);
+
+   ecore_con_client_timeout_set(ev->client, 6);
+
+   ecore_con_client_data_set(ev->client, client);
+
+   srv = ecore_con_client_server_get(ev->client);
+   printf("Clients connected to this server:\n");
+   clients = ecore_con_server_clients_get(srv);
+   EINA_LIST_FOREACH(clients, l, cl)
+     printf("%s\n", ecore_con_client_ip_get(cl));
+
+   return ECORE_CALLBACK_RENEW;*/
+}
+
+
 
 void main(void)
 {
-   Ecore_Con_Server *svr;
+    // Our server
+    Ecore_Con_Server *svr ;
 
-   ecore_init();
-   ecore_con_init();
+    // Init Ecore
+    ecore_init();
+    ecore_con_init();
 
-   if (!(svr = ecore_con_server_add(ECORE_CON_REMOTE_TCP, "127.0.0.1", 1234, NULL)))
+    // Start Server
+    if (!(svr = ecore_con_server_add(ECORE_CON_REMOTE_TCP, "127.0.0.1", 1234, NULL)))
             exit(1);
 
-printf("Server done !\n") ;
-
+    // Set timeout and client limit
     ecore_con_server_timeout_set(svr, 10);
     ecore_con_server_client_limit_set(svr, 3, 0);
 
+    printf("Server done !\n") ;
+
+    // What it doing when new client, client quit, or data received
+    ecore_event_handler_add(ECORE_CON_EVENT_CLIENT_ADD, (Ecore_Event_Handler_Cb)_add, NULL);
+//    ecore_event_handler_add(ECORE_CON_EVENT_CLIENT_DEL, (Ecore_Event_Handler_Cb)_del, NULL);
+//    ecore_event_handler_add(ECORE_CON_EVENT_CLIENT_DATA, (Ecore_Event_Handler_Cb)_data, NULL);
+
+    // Start ecore loop
     ecore_main_loop_begin();
 
 
